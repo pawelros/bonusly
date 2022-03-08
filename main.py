7d5c7d6e3c8fb2e81b99f2232485745119361dc3
Pawel Rosinski <pawel.rosinski@blockfi.com>
2022-03-09 00:37:44 +0100

    It is working

--- main.py
+++ main.py
@@ -1,52 +1,118 @@
 import argparse
+from dotenv import dotenv_values
+import json
+import random
 import sys
+import urllib3
 
-def main():
-    pass
+from reasons import Reasons
 
-if __name__ == '__main__':
+
+def main(to, amount, reason, hashtag, api_token):
+
+    body = {}
+
+    body["receiver_email"] = to
+    body["amount"] = amount
+    body["hashtag"] = hashtag
+    body["reason"] = reason
+
+    j = json.dumps(body)
+
+    print(j)
+
+    # Creating a PoolManager instance for sending requests.
+    http = urllib3.PoolManager()
+    resp = http.request(
+        "POST",
+        "https://bonus.ly/api/v1/bonuses",
+        body=j,
+        headers={"Content-Type": "application/json", "Authorization": f"Bearer {api_token}"}
+    )
+
+    print(resp.data)
+
+
+def random_hashtag():
+    tags = [
+        "#clients-not-customers",
+        "#individual-effort-collective-success",
+        "#pragmatic-pioneering",
+        "#transparency-builds-trust-tbt",
+    ]
+
+    index = random.randrange(0, len(tags))
+
+    return tags[index]
+
+
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
-        description='Give bonusly points to your colleagues'
+        description="Give bonusly points to your colleagues"
     )
-    parser.add_argument(
-        '--amount',
-        default='1',
-        help='The number of points you want to give away',
-    )
-    parser.add_argument(
-        '--amount-random-min',
-        default='1',
-        help='The number of minimum points you want to give away using random number generator. Must be used with --amount-random-max',
-    )
-    parser.add_argument(
-        '--amount-random-max',
-        default='1',
-        help='The number of maximum points you want to give away using random number generator.  Must be used with --amount-random-min',
-    )
-    parser.add_argument(
-        '--round',
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "--amount",
         default=None,
-        help='Allowed values [5|10]. If specified, randomly generated amount will be rounded. --amount-random-min must be >= --round value.',
+        type=int,
+        help="The number of points you want to give away",
+    )
+    group.add_argument(
+        "--max",
+        default="20",
+        type=int,
+        help="The number of maximum points you want to give away using random number generator.",
     )
     parser.add_argument(
-        '--to',
+        "--round",
         default=None,
+        type=int,
+        choices=[5, 10],
+        help="If specified, randomly generated amount will be rounded to the given precision. i.e n=23; --round 5 will be rounded to 25, --round 10 to 20.",
+    )
+    parser.add_argument(
+        "--to",
+        default=None,
+        required=True,
         help='[ a comma separated list of receiver emails i.e. "jp@example.com,monica@example.com"',
     )
     parser.add_argument(
-        '--hashtag',
-        default='random',
-        help='Hashtag [ random | #clients-not-customers | #individual-effort-collective-success | #pragmatic-pioneering | #transparency-builds-trust-tbt ] ',
+        "--hashtag",
+        default=None,
+        choices=[
+            "clients-not-customers",
+            "individual-effort-collective-success",
+            "pragmatic-pioneering",
+            "transparency-builds-trust-tbt",
+        ],
+        help="Hashtag to use",
     )
     parser.add_argument(
-        '--reason',
-        default='random',
-        help='Specific reason or random one',
+        "--reason",
+        default="random",
+        help="Specific reason or random one",
     )
 
     args = parser.parse_args()
 
-    print("## YOLO!")
-    main()
+    random_step = args.round or 1
+    amount = args.amount or random.randrange(random_step, args.max, random_step)
+    hashtag = args.hashtag or random_hashtag()
+    
+    reason = args.reason
+    if reason == "random":
+        reason = Reasons.random()
 
+    if hashtag[0] != "#":
+        hashtag = "#" + hashtag
+
+    if "@" not in args.to:
+        print("--to must be an email")
+        sys.exit(1)
+
+    config = dotenv_values(".env")
+    api_token = config["BONUSLY_API_TOKEN"]
+
+    print("## THE THINGS I DO FOR TEAM:")
+    main(args.to, amount, reason, hashtag, api_token)
---
+++ reasons.py
@@ -0,0 +1,35 @@
+import random
+class Reasons:
+    @staticmethod
+    def random():
+        reasons = [
+            'Great presentation about CI/CD today!',
+            'For being always ready to investigate infrastructure mysteries',
+            'For being always ready to investigate infrastructure mysteries',
+            'For all your work on stack manager.',
+            'Nice work on the pulumi code changes! Looking forward to seeing this leveraged in our environment.',
+            'thx for all your support!!',
+            'nothing could have been possible without you!',
+            'everyday is a new day with all your amazing work!',
+            'thx again for all the time you pass on helping us!!',
+            'Good presentation today!',
+            'Friday\'s round table was absolutely great!',
+            'For the great brainstorming session',
+            'For harsh words that somethimes had to be told.',
+            'for the thoughtful discussions within the team',
+            'Smashing!',
+            'thx for being an incredible team member !!!',
+            'For daily handling of Cyclope with great sense of good humour',
+            'for being a pulumi guru!',
+            'Awesome work on bender',
+            'best engineer I know',
+            'Rapid turnaround on a high impact issue',
+            'for being one man army and fixing all the things!',
+            'for helping me in solving cache problem in github workflow.',
+            'Thank you for helping with the release',
+            'Thank you for helping to identify this nasty bug!'
+        ]
+
+        index = random.randrange(0, len(reasons))
+
+        return reasons[index]
